cd96b00f343ed8d86a4cfc6890eb0553c54bfd29
puru-soni-04 <purusoni@buffalo.edu>
2023-08-16 20:12:14 -0400

    added tfjs model

--- JS_MobileNetV2_testing.js
+++ JS_MobileNetV2_testing.js
@@ -1,4 +1,5 @@
-import * as tf from '@tensorflow/tfjs';
+import * as tf from '@tensorflow/tfjs-node';
 
-const model = await tf.loadGraphModel('./JS_MobileNetV2_fine_tuned/model.json');
-model.summary();
+const model = await tf.loadGraphModel('file://./JS_MobileNetV2_fine_tuned/model.json');
+
+model.predict('file://./extra_unlabeled_test_images/B.jpg');
